bcef5525193b83f358a493647beb413eb2510b67
Sunny F <doc.inkwell@gmail.com>
2017-05-06 23:57:35 -0400

    made file opening and closing vorbose

--- parser.py
+++ parser.py
@@ -3,12 +3,18 @@ import time
 
 with open('nowplaying.txt', 'r+') as nowplaying:
 	while True:
-		playback = json.loads(open('C:\Users\Sunny\AppData\Roaming\Google Play Music Desktop Player\json_store\playback.json').read())
+		#print 'opening'
+		json_data = open('C:\Users\Sunny\AppData\Roaming\Google Play Music Desktop Player\json_store\playback.json')
+		#print 'reading'
+		playback = json.loads(json_data.read())
 		title = str(playback['song']['title']).lstrip('u')
 		artist = str(playback['song']['artist']).lstrip('u')
 		track = '| {} - {} |'.format(title, artist)
 		nowplaying.seek(0)
+		#print 'writing {}'.format(track)
 		nowplaying.write(track)
+		#print 'truncating'
 		nowplaying.truncate()
-		#print 'writing'
+		#print 'closing'
+		json_data.close()
 		time.sleep(5)
