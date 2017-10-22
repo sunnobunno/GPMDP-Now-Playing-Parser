81506599aa2edea0ca599d478296aea9c3f648e5
Sunny F <doc.inkwell@gmail.com>
2017-10-22 12:50:45 -0400

    idk

--- parser.py
+++ parser.py
@@ -3,18 +3,18 @@ import time
 
 with open('nowplaying.txt', 'r+') as nowplaying:
 	while True:
-		#print 'opening'
+		print 'opening'
 		json_data = open('C:\Users\Sunny\AppData\Roaming\Google Play Music Desktop Player\json_store\playback.json')
-		#print 'reading'
+		print 'reading'
 		playback = json.loads(json_data.read())
 		title = str(playback['song']['title']).lstrip('u')
 		artist = str(playback['song']['artist']).lstrip('u')
 		track = '| {} - {} |'.format(title, artist)
 		nowplaying.seek(0)
-		#print 'writing {}'.format(track)
+		print 'writing {}'.format(track)
 		nowplaying.write(track)
-		#print 'truncating'
+		print 'truncating'
 		nowplaying.truncate()
-		#print 'closing'
+		print 'closing'
 		json_data.close()
 		time.sleep(5)
